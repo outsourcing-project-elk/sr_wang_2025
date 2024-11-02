714697c610a83b2edf2a4c61a322a90a1325954b
Jiezhou <jiezhou8917@gmail.com>
2024-11-02 15:00:52 +0800

    init

---
+++ backend/src/main/java/com/lhqjlb/project/mapper/UserrMapper.java
@@ -0,0 +1,29 @@
+package com.lhqjlb.project.mapper;
+
+import cn.hutool.core.lang.Dict;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.yulichang.base.MPJBaseMapper;
+import com.lhqjlb.project.entity.Userr;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * 用户
+ */
+@Mapper
+public interface UserrMapper extends MPJBaseMapper<Userr> {
+
+    Userr login(Userr user);
+
+    List<Userr> listByUsername(Dict params);
+
+    Page<Userr> page(Page<Userr> page, Dict params);
+
+
+    List<Userr> listOptions();
+
+    void deleteMany(Dict params);
+
+}
